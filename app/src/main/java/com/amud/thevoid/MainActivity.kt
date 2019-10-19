ac8cc6ea6595f2d2f047cfd2fa26da16d5aa61aa
amud <ankur.mudgal.iiitm@gmail.com>
2019-10-19 19:46:50 +0530

    fake 10

--- app/src/main/java/com/amud/thevoid/MainActivity.kt
+++ app/src/main/java/com/amud/thevoid/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // fake 9
+        // fake 10
     }
 }
